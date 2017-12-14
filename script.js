63039c0adb8193e9c1dd3cdff712a97c577a21d1
Teya <dlechuga30@gmail.com>
2017-12-13 22:23:48 -0500

    Assignment to demonstrate jQuery hide and show

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+ $('img').click(function(){
+   $(this).hide(1000);
+  });
+});
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("img").show(1000);
+  });
+});
